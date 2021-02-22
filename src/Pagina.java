e704c228fa8ffadb6513bbaa43dda9e4914a55e7
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-02-22 15:02:32 -0300

    Inicio criacao Classe Pagina, Dado

--- src/Pagina.java
+++ src/Pagina.java
@@ -1,4 +1,13 @@
 
 public class Pagina {
 
+	private int grau;
+	private Dado chaves[];
+	private Pagina filhos[];
+	
+	public Pagina(int grau) {
+		this.grau = grau;
+		chaves = new Dado[grau];
+		filhos = new Pagina[grau+1];
+	}
 }
